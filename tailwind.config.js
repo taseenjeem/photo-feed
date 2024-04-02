06538ca02cbafe4a7aa619eed56fbf611fc305df
taseenjeem <taseenjeem.dev@gmail.com>
2024-04-03 00:55:46 +0600

    1st

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      extend: {
+        container: {
+          center: true,
+          padding: {
+            DEFAULT: "2rem",
+          },
+        },
       },
     },
   },
